384ae463fc40c3f38b8737a68ee5dcc8952e6a33
WaseemSR <waseemrehman88@gmail.com>
2025-07-15 13:01:28 +0100

    Created Movie Model/Class

--- lib/movie.py
+++ lib/movie.py
@@ -5,4 +5,8 @@ class Movie:
         self.release_year = release_year
         self.genre = genre
     
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
     
+    def __repr__(self):
+        return f"Movie({self.id}, {self.title}, {self.release_year}, {self.genre})"
--- tests/test_movie.py
+++ tests/test_movie.py
@@ -6,3 +6,12 @@ def test_movie_initialises():
     assert movie.title == 'Jurrasic Park'
     assert movie.release_year == 1993
     assert movie.genre == 'sci-fi'
+
+def test_movies_are_eq():
+    movie = Movie(1, 'Jurrasic Park', 1993, 'sci-fi')
+    movie2 = Movie(1, 'Jurrasic Park', 1993, 'sci-fi')
+    assert movie == movie2
+
+def test_movie_is_formatted_correctly():
+    movie = Movie(1, 'Jurrasic Park', 1993, 'sci-fi')
+    assert str(movie)  == "Movie(1, Jurrasic Park, 1993, sci-fi)"
