ee7ec6c45d317c23be0bc0ebb5b8deebc26b12fe
WaseemSR <waseemrehman88@gmail.com>
2025-07-16 15:25:15 +0100

    styling Index page

--- lib/movie_routes.py
+++ lib/movie_routes.py
@@ -4,12 +4,20 @@ from lib.movie import Movie
 from lib.movie_repo import MovieRepository
 
 def apply_movie_routes(app):
+    # @app.route('/movies', methods = ['GET'])
+    # def get_movie():
+    #     connection = get_flask_database_connection(app)
+    #     repository = MovieRepository(connection)
+    #     movies = repository.all()
+    #     return render_template('index.html', movies = movies)
+    
+
     @app.route('/movies', methods = ['GET'])
     def get_movie():
         connection = get_flask_database_connection(app)
         repository = MovieRepository(connection)
         movies = repository.all()
-        return render_template('index.html', movies = movies)
+        return render_template('test_index.html', movies = movies)
     
     @app.route('/movies', methods = ['POST'])
     def post_movie():
@@ -18,3 +26,5 @@ def apply_movie_routes(app):
         movie = Movie(None, request.form ['title'], request.form ['release_year'], request.form ['genre'])
         movie = repository.add(movie)
         return 'Movie added successfully'
+    
+    
